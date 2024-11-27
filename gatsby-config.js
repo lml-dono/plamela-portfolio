18350b455e98c3d24a4d1e870071f043a567382b
Pablo Lamela <pablo.lamela50@gmail.com>
2024-11-27 22:09:31 +0100

    Added site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pablo Lamela - UX/UI Designer`,
+    description: `Fusiono tecnología y diseño para crear experiencias que conectan y sorprenden.`,
+    author: `@pablolamela`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
